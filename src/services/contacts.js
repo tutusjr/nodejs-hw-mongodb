aebb9092ee346667927a8c93ae3fdc7e5c17e7b6
Mustafa_Tutus <tutusfullstack@gmail.com>
2025-02-15 18:47:52 +0300

    half way

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,5 +1,5 @@
 import { SORT_ORDER } from '../constants/index.js';
-import { Contact } from '../models/contacts.js';
+import { Contact } from '../db/models/contacts.js';
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 export const getAllContacts = async ({
   page = 1,
