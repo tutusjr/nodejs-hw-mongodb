2283ea19bafe44d8fb935ea5b17bf44cd989a4a3
Mustafa_Tutus <tutusfullstack@gmail.com>
2025-03-04 18:59:32 +0300

    env adjusted

--- src/constants/index.js
+++ src/constants/index.js
@@ -19,6 +19,7 @@ export const SMTP = {
   SMTP_FROM: 'SMTP_FROM',
 };
 
+
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
 
